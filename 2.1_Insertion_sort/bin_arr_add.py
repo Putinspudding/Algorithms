c5b6859a3ac1816790b8643924b3556e12e76331
Putinspudding <Putinspudding@users.noreply.github.com>
2020-01-21 17:48:57 +0800

    Initial commit

---
+++ 2.1_Insertion_sort/bin_arr_add.py
@@ -0,0 +1,15 @@
+def bin_add(a: list, b: list) -> list:
+    i = len(a) - 1
+    c = [0] * (i + 2)
+    while (i >= 0):
+        c[i + 1] = a[i] + b[i] + c[i + 1]
+        if c[i + 1] >= 2:
+            c[i + 1] = c[i + 1] - 2
+            c[i] = c[i] + 1
+        i = i - 1
+    return c
+
+
+a = [1, 1, 1]
+b = [0, 0, 1]
+print(bin_add(a, b))
