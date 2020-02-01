2b91669289d31a0194dce2a39c7982adf23cda29
Putinspudding <Putinspudding@users.noreply.github.com>
2020-02-01 19:10:24 +0800

    Update randomized_select.py

--- 9_Median_And_Order_Statistic/9.2_Randomized_Select/randomized_select.py
+++ 9_Median_And_Order_Statistic/9.2_Randomized_Select/randomized_select.py
@@ -1,5 +1,7 @@
 import random
-def randomized_partition(A:list,begin:int,end:int):
+
+
+def randomized_partition(A: list, begin: int, end: int):
     number = random.randint(begin, end)
     A[number], A[end] = A[end], A[number]
     sample = A[end]
@@ -13,18 +15,18 @@ def randomized_partition(A:list,begin:int,end:int):
     pass
 
 
-def randomized_select(A:list,begin:int,end:int,loc:int):
-    if begin == end :
+def randomized_select(A: list, begin: int, end: int, loc: int):
+    if begin == end:
         return A[begin]
-    comparsion = randomized_partition(A,begin,end)
+    comparsion = randomized_partition(A, begin, end)
     present_loc = comparsion - begin + 1
     if loc == present_loc:
         return A[comparsion]
-    elif loc<present_loc:
-        return randomized_select(A,begin,comparsion-1,loc)
+    elif loc < present_loc:
+        return randomized_select(A, begin, comparsion - 1, loc)
     else:
-        return randomized_select(A,comparsion+1,end,loc-present_loc)
+        return randomized_select(A, comparsion + 1, end, loc - present_loc)
 
 
-a = [1,3,2,5,9,4]
-print(randomized_select(a,0,5,4))
+a = [1, 3, 2, 5, 9, 4]
+print(randomized_select(a, 0, 5, 4))
